29f759e0b344255cc27bcbd38dc773efbfab959d
Changaco <changaco@changaco.net>
2012-09-01 22:00:02 +0200

    add possibility to provide settings in the command line

--- media_info/__main__.py
+++ media_info/__main__.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, unicode_literals
+
 import argparse
 import json
 try:
@@ -7,11 +8,8 @@ except ImportError:
     jsonpickle = None
 
 from . import MediaInfo, MediaInfoException
+from .utils import urldecode
 
-p = argparse.ArgumentParser()
-p.add_argument('url')
-p.add_argument('-b', '--backends', nargs='*', default=None)
-args = p.parse_args()
 
 class FakeEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -20,12 +18,28 @@ class FakeEncoder(json.JSONEncoder):
         except TypeError:
             return str(obj)
 
-media_info = MediaInfo(backends=args.backends)
+
+p = argparse.ArgumentParser()
+p.add_argument('uri')
+p.add_argument('settings', nargs='?', default='', help='urlencoded settings, e.g. "soundcloud:client_id=foo"')
+p.add_argument('-b', '--backends', nargs='*', default=None)
+args = p.parse_args()
+
+
+settings = {}
+for k, v in urldecode(args.settings).items():
+    backend, setting = k.split(':', 1)
+    settings.setdefault(backend, {})
+    settings[backend][setting] = v
+
+media_info = MediaInfo(backends=args.backends, settings=settings)
 try:
-    info = media_info.get(args.url)
-    if jsonpickle:
-        print(json.dumps(json.loads(jsonpickle.encode(info.__dict__)), indent=4))
-    else:
-        print(json.dumps(info.__dict__, indent=4, cls=FakeEncoder))
+    info = media_info.get(args.uri)
 except MediaInfoException as e:
     print(e.args[0])
+    exit(1)
+
+if jsonpickle:
+    print(json.dumps(json.loads(jsonpickle.encode(info.__dict__)), indent=4))
+else:
+    print(json.dumps(info.__dict__, indent=4, cls=FakeEncoder))
